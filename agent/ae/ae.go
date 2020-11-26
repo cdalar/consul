9e56d33ccc764606be5ea316512bf5e5b0b51623
Daniel Nephin <dnephin@hashicorp.com>
2020-11-26 14:30:58 -0500

    ae: unexport Paused as isPaused

--- agent/ae/ae.go
+++ agent/ae/ae.go
@@ -3,12 +3,14 @@ package ae
 
 import (
 	"fmt"
-	"github.com/hashicorp/consul/lib"
-	"github.com/hashicorp/consul/logging"
-	"github.com/hashicorp/go-hclog"
 	"math"
 	"sync"
 	"time"
+
+	"github.com/hashicorp/go-hclog"
+
+	"github.com/hashicorp/consul/lib"
+	"github.com/hashicorp/consul/logging"
 )
 
 // scaleThreshold is the number of nodes after which regular sync runs are
@@ -169,7 +171,7 @@ func (s *StateSyncer) runFSM(fs fsmState, next func(fsmState) fsmState) {
 func (s *StateSyncer) nextFSMState(fs fsmState) fsmState {
 	switch fs {
 	case fullSyncState:
-		if s.Paused() {
+		if s.isPaused() {
 			return retryFullSyncState
 		}
 
@@ -199,7 +201,7 @@ func (s *StateSyncer) nextFSMState(fs fsmState) fsmState {
 			return fullSyncState
 
 		case syncChangesNotifEvent:
-			if s.Paused() {
+			if s.isPaused() {
 				return partialSyncState
 			}
 
@@ -322,7 +324,7 @@ func (s *StateSyncer) Pause() {
 }
 
 // Paused returns whether sync runs are temporarily disabled.
-func (s *StateSyncer) Paused() bool {
+func (s *StateSyncer) isPaused() bool {
 	s.pauseLock.Lock()
 	defer s.pauseLock.Unlock()
 	return s.paused != 0
